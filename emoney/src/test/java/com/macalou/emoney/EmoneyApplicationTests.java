4d0dfc583ea7d7947b8941be8ce1fba372188790
macalou01@wsl <macalou01@gmail.com>
2022-03-01 14:58:30 +0000

    commit

---
+++ emoney/src/test/java/com/macalou/emoney/EmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macalou.emoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
